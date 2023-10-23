3dc5d142fc85e859085d9138a950c83534abed75
Mpalambonisi <mpalambonisi63@gmail.com>
2023-10-23 10:27:50 +0200

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+import java.io.IOException;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
 
+        Scanner scr = new Scanner(System.in);// create a Scanner object
+        // ask user for their wieght on earth
+        System.out.println("Please enter your weight on Earth -- {and press enter} :");
+
+        // declaring all variables
+        float weightMpala = scr.nextFloat();
+        double dblResultMpala;
+        int intResultMpala, intFinalMpala;
+        char charResultMpala;
+
+        // operations
+        dblResultMpala = weightMpala * .38; // calculating user weight in Mars
+
+        System.out.println("Your weight on Earth is: " + weightMpala + "kg.\nYour weight on Mars is : "+ dblResultMpala + "kg");
     }
 }
